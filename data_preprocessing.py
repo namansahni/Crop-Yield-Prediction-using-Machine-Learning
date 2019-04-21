168e7d6a0f31acaad290469ceb9d5ad2ce9e5904
namansahni <naman.sahni10@gmail.com>
2019-04-21 19:23:29 +0530

    Add files via upload

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -9,18 +9,35 @@ import pandas as pd
 dataset2013 = pd.read_csv('wheat-2013-supervised.csv')
 dataset2014 = pd.read_csv('wheat-2014-supervised.csv')
 
-dataset = pd.merge(dataset2013, dataset2014, on=['Latitude', 'Longitude'], suffixes=('_2013','2014'))
+#dataset = pd.merge(dataset2013, dataset2014, on=['Latitude', 'Longitude'], suffixes=('_2013','2014'), how='inner')
 
-"""X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
-"""
-"""
+
+X = dataset2013.iloc[:,[9, 18, 19]].values
+y = dataset2013.iloc[:, 25].values
+
+'''
 # Taking care of missing data
 from sklearn.impute import SimpleImputer 
 imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+imputer = imputer.fit(X[:, 9, 17, 18, 19])
+X[:, [9, 17, 18, 19]] = imputer.transform(X[:, [9, 17, 18, 19]])
+'''
 
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+
+"""
 # Encoding categorical data
 # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -32,15 +49,12 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 """
-"""
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-"""
+
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train)
+"""
